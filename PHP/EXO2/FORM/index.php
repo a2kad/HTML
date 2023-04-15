cfa746a6ff371254e6fef3de1ea865ae2cc6e88a
a2kad <reshetnikovruslan705@gmail.com>
2023-04-15 15:28:53 +0200

    Preparation of form

---
+++ PHP/EXO2/FORM/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="/PHP/EXO2/FORM/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="main">
+            <div class="header">
+                <h1>Contacts</h1>
+            </div>
+            <div class="form">
+                <form action="" method="post">
+                    <label for="nom">Nom :</label><br>
+                    <input class="inp" type="text" name="" id="nom">
+                    <br>
+                    <label for="prenom">Prenom :</label><br>
+                    <input class="inp" type="text" name="" id="prenom">
+                    <br>
+                    <label for="mobile">Tél. mobile :</label><br>
+                    <input class="inp" type="tel" name="" id="mobile">
+                    <br>
+                    <fieldset>
+                        <legend>Secteurs d'activité</legend>
+                        <div class="secteurs">
+                            <input class="radio" type="radio" name="secteurs" id="agro" value="agro" checked>
+                            <label for="agro">Agroalimentaire</label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="banque" value="banque">
+                            <label for="banque">Banque / Assurance</label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="btp" value="btp">
+                            <label for="btp">BTP / Matériaux de construction</label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="commerce" value="commerce">
+                            <label for="commerce">Commerce / Négoce / Distribution </label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="electro" value="electro">
+                            <label for="electro">Électronique / Électricité </label>
+                            <br>
+                            <input class="radio" type="radio" name="telecom" id="banque" value="telecome">
+                            <label for="telecom">Informatique / Télécoms </label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="auto" value="auto">
+                            <label for="auto">Machines et équipements / Automobile</label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="textil" value="textil">
+                            <label for="textil">Textile / Habillement / Chaussure </label>
+                            <br>
+                            <input class="radio" type="radio" name="secteurs" id="transport" value="transport">
+                            <label for="transport">Transports / Logistique</label>
+                            <br>
+                        </div>
+                    </fieldset>
+                    <p>Description :</p>
+                    <textarea name="desc" id="desc" cols="30" rows="10"></textarea>
+                    <br>
+                    <label for="pass">Mot de passe :</label><br>
+                    <input class="inp" type="password" name="pass" id="pass">
+                    <br>
+                    <button class="btn submit" type="submit">Validé</button>
+                    <button class="btn reset" type="reset">Annulé</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
