78585f1502d49ed49d49b1f12466b08d21b718ff
a2kad <reshetnikovruslan705@gmail.com>
2023-04-17 13:03:31 +0200

    JS exo

--- JS/scripts/script.js
+++ JS/scripts/script.js
@@ -1,5 +1,17 @@
 function bonjour(){
-    var message = "Bonjour tout le monde!";
+    var message = "Bonjour tout le monde!<br>";
     document.write(message);
 }
 bonjour();
+function age(pn,age){
+    document.write(pn+" a "+age+" ans.<br>");
+}
+age ("Patrick", 23);
+age("Nicolas", 44);
+age("Moussa", 29);
+
+var voiture = [ "BMW", "Audi", "Saab"];
+
+document.write(voiture[0] + "<br>");
+document.write(voiture[1] + "<br>");
+document.write(voiture[2] + "<br>");
